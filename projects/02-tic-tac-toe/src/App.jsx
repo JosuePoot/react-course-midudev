104902e248d4588b8e2d5842e8f55f2c9c0fd47a
Josué Francisco Poot Pool <josuepootpool@gmail.com>
2025-09-25 02:03:53 -0600

    feat: add canvas-confetti for celebratory effects on game win

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import confetti from 'canvas-confetti'
 
 const TURNS = {
   X: 'x',
@@ -80,6 +81,7 @@ function App() {
     //revisar si hay ganador
     const newWinner = checkWinner(newBoard)
     if (newWinner) {
+      confetti()
       setWinner(newWinner)
     } else if (checkEndGame(newBoard)) {
       setWinner(false) //empate
